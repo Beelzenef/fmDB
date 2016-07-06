7ca822a1fff4c1a17e7b029d891a8bcbc6fc502d
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-06 13:17:31 +0200

    Agregando comando generico

---
+++ fmdb/fmdb/CGenerico.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+using System;
+
+namespace WPF_Canciones
+{
+    class ComandoGenerico : ICommand
+    {
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public ComandoGenerico(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this._canExecute = canExecute;
+            this._execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event System.EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
