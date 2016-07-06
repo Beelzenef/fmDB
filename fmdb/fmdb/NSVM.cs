7dc8baeb46ad5ce4cf93b66c080a64128dfd1cd4
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-06 13:16:16 +0200

    Estructura VM

--- fmdb/fmdb/NSVM.cs
+++ fmdb/fmdb/NSVM.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace fmdb
 {
-    class NSVM
+    class NSVM : INotifyPropertyChanged
     {
+        #region Campos e instancias
+
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Metodos
+
+        #endregion
+
+        #region Comandos
+
+        #endregion
+
+        #region Manejador y metodo para notificacion de cambios en las propiedades
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotificarCambiosEnPropiedad(string propiead)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propiead));
+        }
+
+        #endregion
+
     }
 }
