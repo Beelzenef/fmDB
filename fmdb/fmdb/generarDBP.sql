39c7a707ac44423ec9bbe824bab5692124231dc2
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-26 19:47:57 +0200

    Script generador de DB

---
+++ fmdb/fmdb/generarDBP.sql
@@ -0,0 +1,19 @@
+﻿sqlite3 filmDBdb;
+
+create table peliculas (
+	id int primary key,
+	titulo text,
+	anio text,
+	genero text,
+	reseniada int,
+	url text,
+	valoracion int
+);
+
+insert into peliculas (
+	titulo, anio, genero, reseniada, url, valoracion
+						)
+	values
+	( 
+	'Vanilla Sky', '2001', 'Ciencia ficcion', 0, 'SIN URL', 10
+	);
