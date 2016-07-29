522a48cc3707400fc883b82431e7a1d1ea09a00a
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-07-29 17:16:12 +0200

    Introduccion de DB y conexion SQLite

--- fmdb/fmdb/DAO.cs
+++ fmdb/fmdb/DAO.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace fmdb
 {
     class DAO
     {
+        public SQLiteConnection conn;
+        string cadenaConexion = "Data Source=" + "filmdb.db" + ";Version=3;" + "FailMissing=true;";
+
+        public bool Conectar()
+        {
+            try
+            {
+                conn = new SQLiteConnection(cadenaConexion);
+                conn.Open();
+                return true;
+            }
+            catch (SQLiteException e)
+            {
+                throw new Exception("Error de conexion: " + e.Data);
+            }
+        }
+
+        public void Desconectar()
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (SQLiteException)
+            {
+                throw;
+            }
+        }
     }
 }
